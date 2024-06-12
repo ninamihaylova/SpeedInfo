598c70d8fe3b0f66c97cdaaec6f72b652e184393
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 11:20:14 +0300

    Initial commit

---
+++ SpeedInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿class SpeedInfo
+{
+    public static void Main(String[] args)
+    {
+        double speed = double.Parse(Console.ReadLine());
+        if (speed <= 30)
+        {
+            Console.WriteLine("Slow");
+        }
+        else
+            {
+                Console.WriteLine("Fast");
+            }
+        }
+    }
